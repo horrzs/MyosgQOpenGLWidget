50f82a6e5ddfffc7a56d1884c8454fd881bf13a3
Wang RuiRui <161914629+horrzs@users.noreply.github.com>
2024-03-12 16:11:35 +0800

    Create MyosgQOpenGLWidget.cpp

---
+++ MyosgQOpenGLWidget.cpp
@@ -0,0 +1,36 @@
+//
+// Created by horrs on 2024/3/8.
+//
+
+#include "include/MyosgQOpenGLWidget.h"
+#include <QMouseEvent>
+#include <osgEarth/LogarithmicDepthBuffer>
+
+MyosgQOpenGLWidget::MyosgQOpenGLWidget(QWidget *parent) : osgQOpenGLWidget(parent) {
+    setMouseTracking(true);
+    setFocus();
+
+    connect(this, &osgQOpenGLWidget::initialized, [=]() {
+        // 【非常关键】设置相机的投影矩阵
+        QSize size = this->sizeHint();
+        float aspectRatio = static_cast<float>(size.width()) / static_cast<float>(size.height());
+        this->getOsgViewer()->getCamera()->setProjectionMatrixAsPerspective(30.f, aspectRatio, 1.f, 1000.f);
+
+        // osgEarth加载shp文件，LineSymbol不显示
+        osgEarth::GLUtils::setGlobalDefaults(this->getOsgViewer()->getCamera()->getOrCreateStateSet());
+
+        // osgearth消除近裁剪平面离物体太近时的裁剪问题
+        osgEarth::LogarithmicDepthBuffer buf;
+        buf.install(this->getOsgViewer()->getCamera());
+    });
+}
+
+QSize MyosgQOpenGLWidget::sizeHint() const {
+    return QSize(this->width(),this->height());
+}
+
+void MyosgQOpenGLWidget::mouseMoveEvent(QMouseEvent *event) {
+//    qDebug() << "MyosgQOpenGLWidget::mouseMoveEvent" << "x: " << event->x() << ",y: " << event->y();
+    emit signalMouseMoveEvent(event);
+    osgQOpenGLWidget::mouseMoveEvent(event);
+}
